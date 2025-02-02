33bf4dac5da15a6f8fa9bf4b249105018973793f
Joseph <joseph@joseph-rock.com>
2025-02-02 13:52:04 -0800

    cargo fmt

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -29,18 +29,29 @@ fn part_1(input: &str) -> usize {
     let mut total: usize = 0;
 
     for line in input.lines() {
-        let instructions: Vec<Instruction> = re.captures_iter(line).map(|inst| {
-            let name = String::new();
-            let left = inst.name("left").unwrap().as_str().parse::<usize>().unwrap();
-            let right = inst.name("right").unwrap().as_str().parse::<usize>().unwrap();
-            Instruction{
-                name,
-                left,
-                right,
-            }
-        }).collect();
+        let instructions: Vec<Instruction> = re
+            .captures_iter(line)
+            .map(|inst| {
+                let name = String::new();
+                let left = inst
+                    .name("left")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap();
+                let right = inst
+                    .name("right")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap();
+                Instruction { name, left, right }
+            })
+            .collect();
 
-        total += instructions.into_iter().fold(0, |total, inst| total + (inst.left * inst.right));
+        total += instructions
+            .into_iter()
+            .fold(0, |total, inst| total + (inst.left * inst.right));
     }
 
     total
@@ -59,16 +70,29 @@ fn part_2(input: &str) -> usize {
 
     let mut instruction_list: Vec<Instruction> = Vec::new();
     for line in input.lines() {
-        let instructions: Vec<Instruction> = re.captures_iter(line).map(|inst| {
-            let name = inst.name("name").unwrap().as_str();
-            let left = inst.name("left").unwrap().as_str().parse::<usize>().unwrap_or(0);
-            let right = inst.name("right").unwrap().as_str().parse::<usize>().unwrap_or(0);
-            Instruction{
-                name: name.to_string(),
-                left,
-                right,
-            }
-        }).collect();
+        let instructions: Vec<Instruction> = re
+            .captures_iter(line)
+            .map(|inst| {
+                let name = inst.name("name").unwrap().as_str();
+                let left = inst
+                    .name("left")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap_or(0);
+                let right = inst
+                    .name("right")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap_or(0);
+                Instruction {
+                    name: name.to_string(),
+                    left,
+                    right,
+                }
+            })
+            .collect();
         instruction_list.extend(instructions);
     }
 
@@ -77,15 +101,13 @@ fn part_2(input: &str) -> usize {
     for inst in instruction_list {
         if inst.name.ends_with("do") {
             flag = true;
-        }
-        else if inst.name.ends_with("don't") {
+        } else if inst.name.ends_with("don't") {
             flag = false;
         }
 
         if flag && inst.name.ends_with("mul") {
             total += inst.left * inst.right;
         }
-        
     }
 
     total
