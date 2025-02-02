3c3aa75067e650365432fca9eaad2e847031e211
Joseph <joseph@joseph-rock.com>
2025-02-02 13:37:48 -0800

    Part 2 solve

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,4 +1,7 @@
-use regex::Regex;
+use regex::{
+    Regex, 
+    RegexSet,
+};
 
 #[derive(Debug)]
 struct Mul {
@@ -6,12 +9,19 @@ struct Mul {
     right: usize,
 }
 
+#[derive(Debug)]
+struct Instruction {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
 fn main() {
     let input: &str = include_str!("./day03.txt");
     let pt1 = part_1(input);
-    // let pt2 = part_2(input);
+    let pt2 = part_2(input);
     println!("Part 1: {pt1}");
-    // println!("Part 2: {pt2}");
+    println!("Part 2: {pt2}");
 }
 
 fn part_1(input: &str) -> usize {
@@ -42,4 +52,48 @@ fn part_1(input: &str) -> usize {
 
     total
 }
- 
+fn part_2(input: &str) -> usize {
+    let re = Regex::new(
+        r"(?x)
+        (?<name>[\w|\']+)
+        (\()
+        (?<left>\d*)
+        ([\,]*)
+        (?<right>\d*)
+        (\))",
+    )
+    .unwrap();
+
+    let mut total: usize = 0;
+    let mut instruction_list: Vec<Instruction> = Vec::new();
+    for line in input.lines() {
+        let instructions: Vec<Instruction> = re.captures_iter(line).map(|inst| {
+            let name = inst.name("name").unwrap().as_str();
+            let left = inst.name("left").unwrap().as_str().parse::<usize>().unwrap_or(0);
+            let right = inst.name("right").unwrap().as_str().parse::<usize>().unwrap_or(0);
+            Instruction{
+                name: name.to_string(),
+                left,
+                right,
+            }
+        }).collect();
+        instruction_list.extend(instructions);
+    }
+
+    let mut flag = true;
+    for inst in instruction_list {
+        if inst.name.ends_with("do") {
+            flag = true;
+        }
+        else if inst.name.ends_with("don't") {
+            flag = false;
+        }
+
+        if flag && inst.name.ends_with("mul") {
+            total += inst.left * inst.right;
+        }
+        
+    }
+
+    total
+}
